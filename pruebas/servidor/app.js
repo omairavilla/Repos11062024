79cb4768f6bd398b0765d073bcfaa4899a688f95
omairavilla <omairavilla@hotmail.com>
2024-06-19 06:24:15 -0500

    no se que es

---
+++ pruebas/servidor/app.js
@@ -0,0 +1,12 @@
+const saludar=require('./modulosfuncionales/saludo')
+
+
+function sumar(a,b)
+{ let resultado;
+    resultado=a+b;
+    return resultado;
+    }
+
+    console.log(sumar(10,35));
+
+    console.log(saludar('Juan'));
